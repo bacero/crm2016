4281a17efb534449c0bcecc1e8a2e9e14cdebca8
Bryant Acero <imbryantacero@gmail.com>
2016-03-21 01:52:53 -0500

    Reportes5

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,6 +6,11 @@ class ReportsController < ApplicationController
 		else
 			@date=DateTime.now.to_date
 		end
-		
+		if Rails.env.development?
+		    @estoy="desarrollo"
+		else
+			@estoy="Produccion"
+		end
+
 	end
 end
