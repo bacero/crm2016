f0a8dd6ae0bacd7811fc9eb5f31ea57a48d32365
Bryant Acero <imbryantacero@gmail.com>
2016-03-21 02:00:04 -0500

    Reportes10

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -7,10 +7,14 @@ class ReportsController < ApplicationController
 			@date=DateTime.now.to_date
 		end
 		if Rails.env.development?
-		    @estoy="desarrollo"
+		    @labor=LaborShift.where("WEEK(created_at)=WEEK('#{@date}') AND labor_id is not null").group(:labor_id).maximum(:id)
+			@aim=LaborShift.where("MONTH(created_at)=MONTH('#{@date}') AND labor_id is null AND project_id  is not null OR  area_aim_id is not null").group(:labor_id,:area_aim_id).maximum(:id)
 		else
-			@estoy="Produccion"
+			@labor=LaborShift.where("EXTRACT(DOW FROM created_at)=EXTRACT(DOW FROM TIMESTAMP '#{@date}') AND labor_id is not null").group(:labor_id).maximum(:id)
+			@aim=LaborShift.where("EXTRACT(MONTH FROM created_at)=EXTRACT(MONTH FROM TIMESTAMP '#{@date}') AND labor_id is null AND project_id  is not null OR  area_aim_id is not null").group(:labor_id,:area_aim_id).maximum(:id)
 		end
 
+		
+		
 	end
 end
